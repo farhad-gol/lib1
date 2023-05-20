e9b2d64863312cb22a9b6d8b60caf3573dd9d35d
FSC <aghajanian@AghaJanian-LT.AkhtarCo.local>
2023-05-20 14:18:43 +0330

    C2

--- lib1/build.gradle.kts
+++ lib1/build.gradle.kts
@@ -44,3 +44,17 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.farhad-gol"
+            artifactId = "lib1"
+            version = "1.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
